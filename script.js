97992f4afb66af8bd1bcffcfdb11fdc2d3c4859c
CatL <catherinelui.2@gmail.com>
2020-07-30 00:09:26 -0600

    got one call forecast to log

--- script.js
+++ script.js
@@ -1,46 +1,63 @@
 //search bar to pull weather forecast from API
 const APIKey = "166a433c57516f51dfab1f7edaed8413";
-let cityName = "";
-let state = "";
+let cityName = "Denver";
 const searchBtn = $("#searchBtn");
 let cities = [];
 
 function geoInfoCall() {
-    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${state}&appid=${APIKey}`;
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`;
 
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        let infoDiv = $("<div class='infoDiv'>");
-        let pCity = $("<p>").text(`Location: ${cityName}, ${state}`);
-        infoDiv.append(pCity);
+        // let infoDiv = $("<div class='infoDiv'>");
+        // let pCity = $("<p>").text(`Location: ${cityName}, ${state}`);
+        // infoDiv.append(pCity);
 
-        let lat = response.city.coord.lat;
-        let pLat = $("<p>").text(lat);
-        let lon = reponse.city.coord.lon;
-        let pLon = $("<p>").text(lon);
-        pCity.append(`${pLat}, ${pLon}`);
-    
+        // let lat = response.city.coord.lat;
+        // let pLat = $("<p>").text(lat);
+        // let lon = reponse.city.coord.lon;
+        // let pLon = $("<p>").text(lon);
+        // pCity.append(`${pLat}, ${pLon}`);
+        console.log(response);
 
-            // Log the resulting object
-            console.log(response);
 
-    
+//forecast.time
+// forecast.time.from Beginning of the period of data forecasted
+// forecast.time.to End of the period of data forecasted
     });
 }
 
 geoInfoCall();
 
 function uvIndexCall() {
+    let lat = "39.7392";
+    let lon = "-104.9847";
     let queryURL =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
     exclude=currnet&appid=${APIKey}`;
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response) {
+        console.log(response);
+
+        
+
+
+    });
+    
+}
+
+uvIndexCall();
     //results display in currentweather div with city name, date, icon of weather cond, temp, humidity, wind spped, UV index
 
     //display 5 days forecast for current city in fiveDayForecast
 
     
-}
+
 
 
 searchBtn.on("click", function() {
