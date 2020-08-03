79b82642573466e940fe614f21fc4c294b19dd74
CatL <catherinelui.2@gmail.com>
2020-08-02 18:56:07 -0600

    Updated daily forecast rendering, formatted script

--- script.js
+++ script.js
@@ -8,12 +8,7 @@ const updateStorage = () => {
     localStorage.setItem(savedCityStorageKey, JSON.stringify(savedCities));
 };
 
-const currentDateTime = moment().format("MMM Do YYYY");
-const forecast1 = moment().add(1, 'days').format("MMM Do");
-const forecast2 = moment().add(2, 'days').format("MMM Do");
-const forecast3 = moment().add(3, 'days').format("MMM Do");
-const forecast4 = moment().add(4, 'days').format("MMM Do");
-const forecast5 = moment().add(5, 'days').format("MMM Do");
+const timeFormat = "MMM Do YYYY";
 
 $("document").ready(function () {
     $("#searchBtn").on("click", function (e) {
@@ -41,11 +36,6 @@ function geoInfoCall(city) {
     $("#cityInput").val("");
     $("#errors").empty();
     $("#currentWeather .card-body").empty();
-    $("#oneDayForecast .card-body").empty();
-    $("#twoDayForecast .card-body").empty();
-    $("#threeDayForecast .card-body").empty();
-    $("#fourDayForecast .card-body").empty();
-    $("#fiveDayForecast .card-body").empty();
     
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
 
@@ -73,7 +63,9 @@ function geoInfoCall(city) {
     });
 }
 
+
 function oneCall(lat, lon) {
+    // API Documentation https://openweathermap.org/api/one-call-api#example
     let queryURL =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&
     exclude=currnet&units=imperial&appid=${APIKey}`;
 
@@ -81,23 +73,22 @@ function oneCall(lat, lon) {
         url: queryURL,
         method: "GET",
     }).then(function(response) {
+        let currentWeather = response.current;
+        let currentDateTime = moment.unix(currentWeather.dt).format(timeFormat);
+        let current = $("<p>").addClass("card-text").text(`Current Weather: ${currentWeather.temp}°F`);
 
-        let current = $("<p>").addClass("card-text").text(`Current Weather: ${response.current.temp}°F`);
+        let currentWeatherIconID = currentWeather.weather[0].icon;
+        let currentWeatherImgURL =`http://openweathermap.org/img/wn/${currentWeatherIconID}@2x.png`;
+        
+        let currentWeatherIcon = $("<img>").attr("src", currentWeatherImgURL);
+        let currentHumidity = $("<p>").addClass("card-text").text(`Humidity: ${currentWeather.humidity}`);
+        let currentWind = $("<p>").addClass("card-text").text(`Wind Speed: ${currentWeather.wind_speed}`);
+        let currentUVI = $("<p>").addClass("uvi").text(`UV Index: ${currentWeather.uvi}`);
 
-        let currentWeather = response.current.weather[0].icon;
-        let imgURL =`http://openweathermap.org/img/wn/${currentWeather}@2x.png`;
-        let icon = $("<img>").attr("src", imgURL);
-
-        let currentHumidity = $("<p>").addClass("card-text").text(`Humidity: ${response.current.humidity}`);
-
-        let currentWind = $("<p>").addClass("card-text").text(`Wind Speed: ${response.current.wind_speed}`);
-
-        let currentUVI = $("<p>").addClass("uvi").text(`UV Index: ${response.current.uvi}`);
-
-        $("#currentWeather .card-body").append(currentDateTime, icon, current, currentHumidity, currentWind, currentUVI);
+        $("#currentWeather .card-body").append(currentDateTime, currentWeatherIcon, current, currentHumidity, currentWind, currentUVI);
 
         //UVI Colors
-        let uvIndex = response.current.uvi;
+        let uvIndex = currentWeather.uvi;
         if (uvIndex <= 2){
             currentUVI.css("background-color", "green");
         } else if (uvIndex <= 5) {
@@ -108,77 +99,32 @@ function oneCall(lat, lon) {
             currentUVI.css("background-color", "red");
         }  
 
-        //forecastOne
-
-
-        let forecastOne = $("<h4>").addClass("card-title").text(forecast1);
-
-        let forecastOneWeatherIcon = response.daily[0].weather[0].icon;
-        let imgURLOne =`http://openweathermap.org/img/wn/${forecastOneWeatherIcon}@2x.png`;
-        let iconOne = $("<img>").attr("src", imgURLOne);
         
-        let forecastOneWeather = $("<p>").addClass("card-text").text(`Temp: ${response.daily[0].temp.day}°F`);
 
-        let forecastOneHumidity = $("<p>").addClass("card-text").text(`Humidity: ${response.daily[0].humidity}`);
+        //forecast
+        const forecastCards = $("#forecastCards");
+        forecastCards.empty();
+        const dailyForecasts = response.daily;
+        const forecastCount = dailyForecasts.length < 5 ? dailyForecasts.length : 5;
+        for (let i = 0; i < forecastCount; i++) {
+            const dailyForecast = dailyForecasts[i];
+            let forecastTimeStamp = moment.unix(dailyForecast.dt).format(timeFormat);
+            let forecastWeatherIcon = dailyForecast.weather[0].icon;
+            let forecastImgURL =`http://openweathermap.org/img/wn/${forecastWeatherIcon}@2x.png`;
 
-        $("#oneDayForecast .card-body").append(forecastOne, iconOne, forecastOneWeather, forecastOneHumidity);
+            let forecastTimeStampEl = $("<h4>").addClass("card-title").text(forecastTimeStamp);
+            let icon = $("<img>").attr("src", forecastImgURL);
+            let forecastTemp = $("<p>").addClass("card-text").text(`Temp: ${dailyForecast.temp.day}°F`);
+            let forecastHumidity = $("<p>").addClass("card-text").text(`Humidity: ${dailyForecast.humidity}`);
+            
+            let forecastCardBody = $("<div>").addClass("card-body");
+            forecastCardBody.append(forecastTimeStampEl, icon, forecastTemp, forecastHumidity);
 
-        //forecastTwo
+            let forecastCard = $("<div>").addClass("card text-white bg-dark mb-3").css("max-width", "18rem");
+            forecastCard.append(forecastCardBody);
 
-        let forecastTwo = $("<h4>").addClass("card-title").text(forecast2);
-
-        let forecastTwoWeatherIcon = response.daily[1].weather[0].icon;
-        let imgURLTwo =`http://openweathermap.org/img/wn/${forecastTwoWeatherIcon}@2x.png`;
-        let iconTwo = $("<img>").attr("src", imgURLTwo);
-        
-        let forecastTwoWeather = $("<p>").addClass("card-text").text(`Temp: ${response.daily[1].temp.day}°F`);
-
-        let forecastTwoHumidity = $("<p>").addClass("card-text").text(`Humidity: ${response.daily[1].humidity}`);
-
-        $("#twoDayForecast .card-body").append(forecastTwo, iconTwo, forecastTwoWeather, forecastTwoHumidity);
-
-        //forecastThree
-
-        let forecastThree = $("<h4>").addClass("card-title").text(forecast3);
-
-        let forecastThreeWeatherIcon = response.daily[2].weather[0].icon;
-        let imgURLThree =`http://openweathermap.org/img/wn/${forecastThreeWeatherIcon}@2x.png`;
-        let iconThree = $("<img>").attr("src", imgURLThree);
-        
-        let forecastThreeWeather = $("<p>").addClass("card-text").text(`Temp: ${response.daily[2].temp.day}°F`);
-
-        let forecastThreeHumidity = $("<p>").addClass("card-text").text(`Humidity: ${response.daily[2].humidity}`);
-
-        $("#threeDayForecast .card-body").append(forecastThree, iconThree, forecastThreeWeather, forecastThreeHumidity);
-
-        //forecastFour
-
-        let forecastFour = $("<h4>").addClass("card-title").text(forecast4);
-
-        let forecastFourWeatherIcon = response.daily[3].weather[0].icon;
-        let imgURLFour =`http://openweathermap.org/img/wn/${forecastFourWeatherIcon}@2x.png`;
-        let iconFour = $("<img>").attr("src", imgURLFour);
-        
-        let forecastFourWeather = $("<p>").addClass("card-text").text(`Temp: ${response.daily[3].temp.day}°F`);
-
-        let forecastFourHumidity = $("<p>").addClass("card-text").text(`Humidity: ${response.daily[3].humidity}`);
-
-        $("#fourDayForecast .card-body").append(forecastFour, iconFour, forecastFourWeather, forecastFourHumidity);
-
-        //Forecast Five
-
-        let forecastFive = $("<h4>").addClass("card-title").text(forecast5);
-
-        let forecastFiveWeatherIcon = response.daily[4].weather[0].icon;
-        let imgURLFive =`http://openweathermap.org/img/wn/${forecastFiveWeatherIcon}@2x.png`;
-        let iconFive = $("<img>").attr("src", imgURLFive);
-        
-        let forecastFiveWeather = $("<p>").addClass("card-text").text(`Temp: ${response.daily[4].temp.day}°F`);
-
-        let forecastFiveHumidity = $("<p>").addClass("card-text").text(`Humidity: ${response.daily[4].humidity}`);
-
-        $("#fiveDayForecast .card-body").append(forecastFive, iconFive, forecastFiveWeather, forecastFiveHumidity);
-
+            forecastCards.append(forecastCard);
+        }
     });
     
 }
